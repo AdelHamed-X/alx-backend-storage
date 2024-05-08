c20a016f6adfe1718bcb3314540325be580b32a8
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-05-08 20:43:16 +0300

    Adding Task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- A SQL script that creates a table users
 
 CREATE TABLE IF NOT EXISTS users(
-	id INT NOT null primary key,
-    email varchar(255) NOT null unique,
-    name varchar(255)
+	id INT NOT NULL primary key,
+    email VARCHAR(255) NOT NULL unique,
+    name VARCHAR(255)
 );
